9b93e02a38da4f805431e753a22356b6e95bf2a6
Sibi Akkash <sibi.akkash@gmail.com>
2020-06-22 12:13:02 +0530

    joining game

--- functions/index.js
+++ functions/index.js
@@ -11,38 +11,64 @@ const playerRef = db.collection('players');
 const gameRef = db.collection('games');
 const commandRef = db.collection('commands');
 
-app.use(cors({ origin: true }));
+app.use(cors());
 app.use(express.json());
 
 app.get('/create-player/:name', async (req, res) => {
     let name = req.params.name;
-    let player = await playerRef.add({ name });
+    let player = await playerRef.add({ 
+        name,
+        msg: "", 
+    });
     console.log(name, player.id);
     res.json({ 'playerID': player.id });
-    // res.send(player.id);
 });
 
-async function isValidPid(req, res, next) {
+async function isValidPlayer(req, res, next) {
     const player = await playerRef.doc(req.params.pid).get();
     if(player.exists) {
         return next();
     } else {
-        next(new Error('PlayerID does not exist'));
+        next(new Error('Player does not exist. Check the player ID'));
     }
 }
 
-app.get('/create-game/:pid', isValidPid, async(req, res) => {
+async function isValidGame(req, res, next) {
+    const game = await gameRef.doc(req.params.gid).get();
+    if(game.exists && game.data().p2_id == null) {
+        return next();
+    } else {
+        next(new Error('Game does not exist. Check the game ID !'));
+    }
+}
+
+app.get('/create-game/:pid', isValidPlayer, async(req, res) => {
     let pid = req.params.pid;
     let game = await gameRef.add({
         p1_id: pid,
         p2_id: null,
 		board: Array(9).fill(''),
         turn: pid,
-		playing: false
+        playing: false,
+        outcome: null   //tie, gameover, forfeit
     });
-    await playerRef.doc(pid).set({ 'gameID': gameID }, { merge: true });
+    await playerRef.doc(pid).set({ 'gameID': game.id }, { merge: true });
     res.json({ 'gameID': game.id });
 });
+
+app.get('/join-game/:gid/player/:pid', isValidPlayer, isValidGame, async(req, res) => {
+    let pid = req.params.pid;
+    let gid = req.params.gid;
+    
+    await gameRef.doc(gid).update({
+        p2_id: pid
+    });
+    await playerRef.doc(pid).set(
+        { 'gameID': gid },
+        { merge: true }
+    );
+    res.json({ 'gameID': gid });
+})
 
 exports.api = functions.https.onRequest(app);
 
--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,23 @@
 
 class Game {
     constructor() {
+        this.board = Array(9).fill('');
+        this.canStartListening = false;
+        this.isMyTurn = false;
+
         this.db = firebase.firestore();
         this.commandsRef = this.db.collection('commands');
         this.playerRef = this.db.collection('players');
         this.gameRef = this.db.collection('players');
+
         this.boardRef = document.getElementById('board');
+        this.joinGameInput = document.getElementById('joinGameID');
         this.createGameBtn = document.getElementById('createGameBtn');
         this.joinGameBtn = document.getElementById('joinGameBtn');
-        this.baseURL = 'https://us-central1-tictactoe-func.cloudfunctions.net/api/';
+
+        this.baseURL = 'https://us-central1-tictactoe-func.cloudfunctions.net/api';
+        // this.baseURL = 'http://localhost:5001/tictactoe-func/us-central1/api';
+        
     }
 
     init() {
@@ -23,21 +32,45 @@ class Game {
         this.joinGameBtn.addEventListener('click', () => this.joinGame());
     }
 
+    startListening() {
+        let stopListening = () => {
+            this.gameRef.doc(this.gameID).onSnapshot(snap => {
+                let updatedBoard = snap.data().board;
+                console.log(snap.data().board);
+                this.setBoard(updatedBoard);
+            });
+        }
+
+        let unsubPlayer = () => {
+            this.playerRef.doc(this.playerID).onSnapshot(snap => {
+                this.setMsg(snap.data().msg);
+            });
+        }
+
+        stopListening();
+        unsubPlayer();
+
+    }
+
     makeBoard() {
-        for(let i = 0; i < 3; i++) {
-            for(let j = 0; j < 3; j++) {
-                let cell = document.createElement('div');
-                cell.setAttribute('id', `cell-${i}-${j}`);
-                cell.addEventListener('click', () => this.makeMove(cell.id));
-                this.boardRef.appendChild(cell);
-            }
+        for(let i = 1; i <= 9; i++) {
+            let cell = document.createElement('div');
+            cell.setAttribute('id', `cell-${i}`);
+            cell.addEventListener('click', () => this.makeMove(i-1));
+            this.boardRef.appendChild(cell);
+        }
+    }
+
+    setBoard(board) {
+        for(let i = 0; i < 9; i++) {
+            document.getElementById(`cell-${i}`).textContent = board[i];
         }
     }
 
     async getPlayerID() {
         if(!this.playerID) {
             this.name = prompt('enter username pls');
-            let response = await fetch(`${this.baseURL}create-player/${this.name}`);
+            let response = await fetch(`${this.baseURL}/create-player/${this.name}`);
             response = await response.json();
             this.playerID = response.playerID;
         }
@@ -45,19 +78,38 @@ class Game {
 
     async createGame() {
         if(this.playerID && !this.gameID) {
-            let response = await fetch(
-                `${this.baseURL}create-game/${this.playerID}`
-            );
+            let response = await fetch( `${this.baseURL}/create-game/${this.playerID}` );
             response = await response.json();
             this.gameID = response.gameID;
         }
         console.log(this.playerID, this.gameID);
+        this.canStartListening = true;
+        this.startListening();
+    }
+
+    async joinGame() {
+        let gameToJoin = this.joinGameInput.value;
+        if(!gameToJoin) {
+            alert('game id cannot be empty');
+        } else {
+            let response = await fetch( 
+                `${this.baseURL}/join-game/${gameToJoin}/player/${this.playerID}`
+            );
+            response = await response.json();
+            this.gameID = response.gameID;
+            console.log(this.playerID, this.gameID);
+        }
+        this.canStartListening = true;
+        this.startListening();
     }
 
     async makeMove(cell) {
-        let doc = this.commandsRef.add({
-            
-        })
+        let cmd = this.commandsRef.add({
+            'type': 'move',
+            'gameID': this.gameID,
+            'playerID': this.playerID,
+            'cellNo': cell
+        });
     }
     
 }
